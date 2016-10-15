27d5dbb217d329d4d57677134d069425e2d6e2e8
yuzhuotengaysy <402890539@qq.com>
2016-10-15 16:07:57 +0800

    配置文件

--- vdoado/admin/index.php
+++ vdoado/admin/index.php
@@ -4,9 +4,9 @@
 
 	//定义常量
 	define("SITE_URL","/");
-	define("CSS_URL",SITE_URL."vdoado/vdoado/Public/css/");
-	define("IMG_URL",SITE_URL."vdoado/vdoado/Public/images/");
-	define("JS_URL",SITE_URL."vdoado/vdoado/Public/js/");
+	define("CSS_URL",SITE_URL."admin/admin/Public/css/");
+	define("IMG_URL",SITE_URL."admin/admin/Public/images/");
+	define("JS_URL",SITE_URL."admin/admin/Public/js/");
 
 	/* 引入框架核心 */
 	include "../ThinkPHP/ThinkPHP.php";
